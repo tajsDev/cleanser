c4e63ec7c75ea1f10f521f8f29c8af9116dbaf00
tajsDev <tomasjauregui14@gmail.com>
2024-10-03 00:21:55 -0700

    added nav agent

--- Scripts/villager_nav.gd
+++ Scripts/villager_nav.gd
@@ -1,11 +1,25 @@
-extends NavigationAgent3D
+extends CharacterBody3D
+@export var player: Node3D
+@export var movement_speed: float = 4.0
+@onready var navigation_agent: NavigationAgent3D = get_node("NavigationAgent3D")
 
+func _ready() -> void:
+	navigation_agent.velocity_computed.connect(Callable(_on_velocity_computed))
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func set_movement_target(movement_target: Vector3):
+	navigation_agent.set_target_position(movement_target)
 
+func _physics_process(delta):
+	if navigation_agent.is_navigation_finished():
+		return
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
+	var new_velocity: Vector3 = global_position.direction_to(next_path_position) * movement_speed
+	if navigation_agent.avoidance_enabled:
+		navigation_agent.velocity = new_velocity
+	else:
+		_on_velocity_computed(new_velocity)
+
+func _on_velocity_computed(safe_velocity: Vector3):
+	velocity = safe_velocity
+	move_and_slide()
