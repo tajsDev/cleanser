605adf95944d4109af782d2cd8d7d11bc5e5cfb9
tajsDev <tomasjauregui14@gmail.com>
2024-10-18 16:39:15 -0700

    added basic bubble

--- Scripts/enemy_hit.gd
+++ Scripts/enemy_hit.gd
@@ -1,11 +1,10 @@
 extends Area3D
-@export var bubble:MeshInstance3D 
+@export var bubble:PackedScene 
 @onready var health_mang = $HealthManager
-
+var bubble_on = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if(bubble != null ):
-		add_child(bubble)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,4 +19,9 @@ func _on_area_entered(area: Area3D) -> void:
 func _on_health_manager_hurts() -> void:
 	if(health_mang.cur_health > 50):
 		health_mang.hurt(10)
+	else:
+		if(bubble != null && !bubble_on):
+			bubble_on = true
+			var bub = bubble.instantiate()
+			add_child(bub)
 	
