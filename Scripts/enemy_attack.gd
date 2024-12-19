c287d594c2f26c667b4919c51a0b1c55f25415de
tajsDev <tomasjauregui14@gmail.com>
2024-12-19 01:31:20 -0700

    added animations to the wolf

--- Scripts/enemy_attack.gd
+++ Scripts/enemy_attack.gd
@@ -3,18 +3,23 @@ extends Node3D
 @export var projectile_scene: PackedScene
 @export var launch_threshold: float = 5.0
 @export var projectile_speed: float = 10.0
+@export var animation: AnimatedSprite3D
 var canAttack = true
 @export var canTimer:Timer
 var isHeal = false
-
+func _ready() -> void:
+	animation.play("walk")
+	
 func _process(delta: float) -> void:
 	var distance_to_target = navigation_agent.distance_to_target()
 	if distance_to_target <= launch_threshold && canAttack && !isHeal:
 		canAttack = false
 		canTimer.start()
 		launch_projectile()
+		
 			
 func launch_projectile() -> void:
+		animation.play("attack")
 		var spell = projectile_scene.instantiate()
 		spell.rotation = global_rotation
 		spell.position = global_position
@@ -27,4 +32,5 @@ func _on_timer_timeout() -> void:
 
 
 func _on_area_3d_changed() -> void:
+	animation.play("changed")
 	isHeal = true
