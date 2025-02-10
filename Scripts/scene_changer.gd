c61d525ee86f4797ed08e9ac0f648bc5e422cca1
tajsDev <tomasjauregui14@gmail.com>
2025-02-09 18:04:38 -0700

    added pixel filter and fixed some bugs

--- Scripts/scene_changer.gd
+++ Scripts/scene_changer.gd
@@ -9,5 +9,6 @@ func moveScene() -> void:
 	get_tree().change_scene_to_file(sceneName)
 
 
-func _on_area_entered(_area: Area3D) -> void:
-	moveScene()
+func _on_area_entered(area: Area3D) -> void:
+	if(area.is_in_group("player")):
+		moveScene()
