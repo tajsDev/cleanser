7f4b42162e80a9ea4ae8946bc59b2a25343424cd
tajsDev <tomasjauregui14@gmail.com>
2025-02-19 14:46:13 -0700

    added the power boost

--- Scripts/attack_book.gd
+++ Scripts/attack_book.gd
@@ -3,19 +3,46 @@ extends Node3D
 var direction
 var speed = 10.0
 @export var cooldown_timer: Timer
+@export var boost_timer : Timer
 @export var action:String
-
+var is_shotgun_mode
 var canShoot = true 
 func _physics_process(_delta):
 	if Input.is_action_just_pressed(action) && canShoot:
 		canShoot = false
 		cooldown_timer.start()
+		if is_shotgun_mode:
+			shoot_shotgun()
+		else:
+			shoot_single()
+
+func shoot_single():
+	var spell = cleanse_spell.instantiate()
+	spell.rotation = global_rotation
+	spell.position = global_position
+	get_tree().get_root().add_child(spell)
+
+func shoot_shotgun():
+	var offset_distance = 1.0 # Adjust as needed to prevent collision
+	var directions = [Vector3.LEFT, Vector3.ZERO, Vector3.RIGHT] # Left, Center, Right
+	
+	for i in range(3): # Spawn 3 projectiles
 		var spell = cleanse_spell.instantiate()
 		spell.rotation = global_rotation
-		spell.position = global_position
+
+		# Offset position slightly to prevent immediate collision
+		spell.position = global_position + (global_transform.basis * directions[i] * offset_distance)
+
 		get_tree().get_root().add_child(spell)
-		spell = null
-	
+
 
 func _on_attack_timer_timeout() -> void:
 	canShoot = true
+
+func _on_player_wolf_spawn():
+	is_shotgun_mode = true
+	boost_timer.start()
+
+
+func _on_boost_timer_timeout():
+	is_shotgun_mode = false
--- Scripts/wolf_spawner.gd
+++ Scripts/wolf_spawner.gd
@@ -1,6 +1,5 @@
 extends Node3D
 @export var child_1: Node3D
-@export var child_2: Node3D
 @export var boost_timer: Timer
 
 func _ready():
@@ -8,11 +7,7 @@ func _ready():
 		
 func _on_player_wolf_spawn():
 	child_1.visible = true
-	child_2.visible = true
-	print(child_1.is_visible_in_tree())
-	print(child_2.is_visible_in_tree())
 	boost_timer.start()
 
 func _on_boost_timer_timeout():
 	child_1.visible = false
-	child_2.visible = false
