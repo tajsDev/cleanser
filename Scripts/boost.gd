43771472f52594d271a6cf49729c13095e5fe602
tajsDev <tomasjauregui14@gmail.com>
2025-03-29 14:28:36 -0700

    make boost for visible

--- Scripts/boost.gd
+++ Scripts/boost.gd
@@ -1,10 +1,11 @@
 extends Node
 @export var boost_ui:Node2D
 @export var boost_time: Timer
-
+@export var animation: AnimatedSprite2D
 
 #this works
 func _on_player_wolf_spawn():
+	animation.play("default")
 	boost_ui.visible = true
 	boost_time.start()
 	
